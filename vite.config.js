8687d93d4f5c3f4d3b94c81aa6e503080003fbf8
RedVortex <58099979+RedVortexDev@users.noreply.github.com>
2024-06-11 22:09:25 +0300

    try to use custom URL paths for pages

--- vite.config.js
+++ vite.config.js
@@ -1,5 +1,6 @@
-import { resolve } from 'path'
-import { defineConfig } from 'vite'
+import {defineConfig} from 'vite';
+import {resolve} from 'path';
+import alias from '@rollup/plugin-alias';
 
 export default defineConfig({
     build: {
@@ -7,7 +8,16 @@ export default defineConfig({
             input: {
                 main: resolve(__dirname, 'index.html'),
                 text_file_to_template: resolve(__dirname, 'pages/text_file_to_template.html'),
+                color_palette: resolve(__dirname, 'pages/color_palette.html'),
             },
+            plugins: [
+                alias({
+                    entries: [
+                        {find: '/color_palette.html', replacement: '/pages/color_palette.html'},
+                        {find: '/text_file_to_template.html', replacement: '/pages/text_file_to_template.html'},
+                    ],
+                }),
+            ],
         },
     },
-})
+});
