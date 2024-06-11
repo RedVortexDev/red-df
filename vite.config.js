3e3f3c0ba66ee4379cc6c2f1aaec18b535ff20dc
RedVortex <58099979+RedVortexDev@users.noreply.github.com>
2024-06-11 22:33:17 +0300

    add color palette to vite inputs

--- vite.config.js
+++ vite.config.js
@@ -1,5 +1,5 @@
-import { resolve } from 'path'
-import { defineConfig } from 'vite'
+import {resolve} from 'path'
+import {defineConfig} from 'vite'
 
 export default defineConfig({
     build: {
@@ -7,6 +7,7 @@ export default defineConfig({
             input: {
                 main: resolve(__dirname, 'index.html'),
                 text_file_to_template: resolve(__dirname, 'pages/text_file_to_template.html'),
+                color_palette: resolve(__dirname, 'pages/color_palette.html'),
             },
         },
     },
