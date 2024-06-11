a3eb2c6955362c37ab355cb80404ba21520c3669
RedVortex <58099979+RedVortexDev@users.noreply.github.com>
2024-06-11 22:31:21 +0300

    revert, someone help xd

--- vite.config.js
+++ vite.config.js
@@ -1,6 +1,5 @@
-import {defineConfig} from 'vite';
-import {resolve} from 'path';
-import alias from '@rollup/plugin-alias';
+import { resolve } from 'path'
+import { defineConfig } from 'vite'
 
 export default defineConfig({
     build: {
@@ -8,16 +7,7 @@ export default defineConfig({
             input: {
                 main: resolve(__dirname, 'index.html'),
                 text_file_to_template: resolve(__dirname, 'pages/text_file_to_template.html'),
-                color_palette: resolve(__dirname, 'pages/color_palette.html'),
             },
-            plugins: [
-                alias({
-                    entries: [
-                        {find: '/color_palette.html', replacement: '/pages/color_palette.html'},
-                        {find: '/text_file_to_template.html', replacement: '/pages/text_file_to_template.html'},
-                    ],
-                }),
-            ],
         },
     },
-});
+})
