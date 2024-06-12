67d2265a420fe5fca4014a7ae25939d5ee5e1ee2
RedVortex <58099979+RedVortexDev@users.noreply.github.com>
2024-06-12 18:44:56 +0300

    reorder files & config

--- vite.config.js
+++ vite.config.js
@@ -1,14 +1,11 @@
-import {resolve} from 'path'
 import {defineConfig} from 'vite'
 
 export default defineConfig({
+    root: 'src',
     build: {
         rollupOptions: {
-            input: {
-                main: resolve(__dirname, 'index.html'),
-                text_file_to_template: resolve(__dirname, 'pages/text_file_to_template.html'),
-                color_palette: resolve(__dirname, 'pages/color_palette.html'),
-            },
+            input: ["src/color_palette.html", "src/text_file_to_template.html", "src/index.html"],
         },
+        outDir: '../dist'
     },
 })
