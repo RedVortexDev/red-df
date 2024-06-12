8f8a98e4c6b0f588515b101a3281284e96b769a8
RedVortex <58099979+RedVortexDev@users.noreply.github.com>
2024-06-12 18:53:39 +0300

    change base

--- vite.config.js
+++ vite.config.js
@@ -2,6 +2,7 @@ import {defineConfig} from 'vite'
 
 export default defineConfig({
     root: 'src',
+    base: '/red-df',
     build: {
         rollupOptions: {
             input: ["src/color_palette.html", "src/text_file_to_template.html", "src/index.html"],
