db21706ef4c7cb1ab6520bd4dda52623f392893a
RedVortex <58099979+RedVortexDev@users.noreply.github.com>
2024-06-14 15:06:04 +0300

    completely unneeded patch note renderer
    this took like 3 hours and I have no idea why I even made this

--- vite.config.js
+++ vite.config.js
@@ -4,7 +4,7 @@ export default defineConfig({
     root: 'src',
     build: {
         rollupOptions: {
-            input: ["src/color_palette.html", "src/text_file_to_template.html", "src/index.html", "src/404.html"],
+            input: ["src/color_palette.html", "src/text_file_to_template.html", "src/index.html", "src/404.html", "src/patch_notes_renderer.html"],
         },
         outDir: '../dist'
     },
