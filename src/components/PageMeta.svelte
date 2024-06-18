49752107cac506e035d82508f86eade7e1ff9fad
RedVortex <58099979+RedVortexDev@users.noreply.github.com>
2024-06-18 17:20:42 +0300

    fix embed when no title

--- src/components/PageMeta.svelte
+++ src/components/PageMeta.svelte
@@ -4,8 +4,8 @@
 </script>
 
 <svelte:head>
-    <title>Red DF{title ? ` | ${title}` : ""}</title>
+    <title>Red DF{title ? ` | ${title}` : ''}</title>
     <meta content="#ce0000" name="theme-color">
-    <meta content="Red DF | {title}" name="title">
+    <meta content="Red DF{title ? ` | ${title}` : ''}" name="title">
     <meta content={description} name="description">
 </svelte:head>
