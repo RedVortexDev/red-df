504692ed0311b60b0694f89f0ca434423fc2ef29
RedVortexDev <58099979+RedVortexDev@users.noreply.github.com>
2025-01-30 22:41:10 +0200

    feat(text-utils): make font previews look better

--- src/routes/text-utils/FontPreview.svelte
+++ src/routes/text-utils/FontPreview.svelte
@@ -1,5 +1,7 @@
-<script>
+<script lang="ts">
     import Button from "$lib/components/ui/button/button.svelte";
+    import {Textarea} from "$lib/components/ui/textarea";
+    import {onMount} from "svelte";
 
     export let fonts = [
         {
@@ -14,16 +16,24 @@
     ];
 
     const sampleText = `Hello, World 1234\n\nLorem ipsum, dolor sit amet consectetur adipisicing elit. Recusandae, reiciendis!`;
+
+    onMount(() => {
+        const textAreas = [...document.querySelectorAll('.text-area')] as HTMLTextAreaElement[];
+        textAreas.forEach(area => area.value = sampleText);
+    });
+
+
 </script>
 
 <div class="flex flex-col items-center p-2">
     <h1 class="font-bold">Small Caps Font</h1>
-    <h2 class="max-w-lg text-center">Made by RedVortex & RaffyRaffy14, maintained by RedVortex.<br>License: LGPLv3-only.</h2>
+    <h2 class="max-w-lg text-center">Made by RedVortex & RaffyRaffy14, maintained by RedVortex.<br>License: LGPLv3-only.
+    </h2>
 </div>
 <div class="grid md:grid-rows-1 md:grid-cols-2 grid-rows-2 grid-cols-1 gap-5">
     {#each fonts as font}
         <div
-            class="bg-zinc-800 p-5 rounded-lg flex items-center md:flex-col h-52 text-wrap gap-2"
+                class="bg-zinc-800 p-5 rounded-lg flex items-center flex-col h-full w-full text-wrap gap-2"
         >
             <div class="flex flex-col gap-2 w-full">
                 <div class="flex items-start">
@@ -32,12 +42,12 @@
                         <span class="bg-cyan-400 text-zinc-900 rounded-sm px-1 py-0 text-sm scale-75 uppercase font-semibold">{font.tag}</span>
                     {/if}
                 </div>
-                <textarea class="{font.fontClass} resize-none bg-none rounded-sm p-1" rows="5" spellcheck="false"
-                    >{sampleText}</textarea
-                >
+                <Textarea dir="auto"
+                          class="{font.fontClass} text-lg leading-6 resize-none bg-none rounded-sm p-3 text-area outline-none bg-zinc-800 transition-all ring-red-600 ring-0 focus-visible:ring-2 focus-visible:bg-zinc-900"
+                          spellcheck="false" rows="8"></Textarea>
             </div>
-            <a href={font.title.replace(" ", "%20") + ".ttf"} download>
-                <Button variant="default" class="mt-5">Download</Button>
+            <a class="w-full" href={font.title.replace(" ", "%20") + ".ttf"} download>
+                <Button variant="default" class="mt-2 w-full">Download</Button>
             </a>
         </div>
     {/each}
